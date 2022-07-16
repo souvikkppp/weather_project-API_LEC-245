57cc0e8d2af6b82c1f2dce43aecc4700f6169c5c
souvikkppp <103935378+souvikkppp@users.noreply.github.com>
2022-07-16 18:30:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+//NODE NATIVE HTTP MODULE TO GET DATA FROM SOMEONE ELSE SERVER
 const https = require("https");
 
 const app = express();
@@ -10,6 +11,8 @@ app.get("/", function(req, res) {
   const url = "https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=a7e320e5e8f944d2e9d0f4b8e17891e2&units=Metric"
   https.get(url, function(response) {
     console.log(response.statusCode);
+    
+    
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);//this json purse hold data in js object
       const icon = weatherData.weather[0].icon;
@@ -20,7 +23,7 @@ app.get("/", function(req, res) {
       res.write("<p>weather description: " + description + "</p>");
       res.write("<h1>The current tempreture is " + temp + "degree celceus</h1>");
       res.write("<img src="+imgUrl+">");
-      res.send()
+      res.send();  //WE CAN USE ONLY ONE RES.SEND IN A GET METHOD THATS WHY WE USE RES.WRITE
     })
   });
 });
